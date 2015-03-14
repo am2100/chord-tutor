a37f07ef971d797ba82e9e92ae496c01a35257c9
am2100 <jimnoble@xjjz.co.uk>
2015-03-14 20:33:12 +0000

    Begin module development

---
+++ ct.js
@@ -0,0 +1,26 @@
+/* jslint settings */
+
+// Module /ct/
+//
+var ct = (function ( $ ) {
+
+  // Module scope variables
+  var
+    // Set constants
+    configMap = { }
+    // Declare all other module scope variables
+    
+  // DOM methods
+
+  // Event handlers
+
+  // Public method /initModule/
+  initModule = function ( $container ) {
+
+    // Render HTML
+    // Initialize
+    // Bind events to event handlers
+  };
+}());
+
+// Start ct once DOM is loaded
